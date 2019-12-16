c6572bda1fac73c9dcf2185021228c1f047387f4
haru <haru.shizu.oki@gmail.com>
2019-12-16 20:14:19 +0900

    Controller Move&NewMap

--- MapGameController.java
+++ MapGameController.java
@@ -1,3 +1,4 @@
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
@@ -11,78 +12,139 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.Group;
 
 public class MapGameController implements Initializable {
-    public MapData mapData;
-    public MoveChara chara;
-    public GridPane mapGrid;
-    public ImageView[] mapImageViews;
-//    public Group[] mapGroups;
+  public MapData mapData;
+  public MoveChara chara;
+  public GridPane mapGrid;
+  public ImageView[] mapImageViews;
+  //    public Group[] mapGroups;
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        mapData = new MapData(21,15);
-        chara = new MoveChara(1,1,mapData);
-//        mapGroups = new Group[mapData.getHeight()*mapData.getWidth()];
-        mapImageViews = new ImageView[mapData.getHeight()*mapData.getWidth()];
-        for(int y=0; y<mapData.getHeight(); y++){
-            for(int x=0; x<mapData.getWidth(); x++){
-                int index = y*mapData.getWidth() + x;
-                mapImageViews[index] = mapData.getImageView(x,y);
-            }
+  /* Mapの初期化 */
+
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    mapData = new MapData(21,15);
+    chara = new MoveChara(1,1,mapData);
+    //        mapGroups = new Group[mapData.getHeight()*mapData.getWidth()];
+    mapImageViews = new ImageView[mapData.getHeight()*mapData.getWidth()];
+    for(int y=0; y<mapData.getHeight(); y++){
+      for(int x=0; x<mapData.getWidth(); x++){
+        int index = y*mapData.getWidth() + x;
+        mapImageViews[index] = mapData.getImageView(x,y);
+      }
+    }
+    mapPrint(chara, mapData);
+  }
+
+  public void mapPrint(MoveChara c, MapData m){
+    int cx = c.getPosX();
+    int cy = c.getPosY();
+    mapGrid.getChildren().clear();
+    for(int y=0; y<mapData.getHeight(); y++){
+      for(int x=0; x<mapData.getWidth(); x++){
+        int index = y*mapData.getWidth() + x;
+        if (x==cx && y==cy) {
+          mapGrid.add(c.getCharaImageView(), x, y);
+        } else {
+          mapGrid.add(mapImageViews[index], x, y);
         }
-        mapPrint(chara, mapData);
+      }
     }
+  }
 
-    public void mapPrint(MoveChara c, MapData m){
-        int cx = c.getPosX();
-        int cy = c.getPosY();
-        mapGrid.getChildren().clear();
-        for(int y=0; y<mapData.getHeight(); y++){
-            for(int x=0; x<mapData.getWidth(); x++){
-                int index = y*mapData.getWidth() + x;
-                if (x==cx && y==cy) {
-                    mapGrid.add(c.getCharaImageView(), x, y);
-                } else {
-                    mapGrid.add(mapImageViews[index], x, y);
-                }
-            }
-        }
-    }
+  public void func1ButtonAction(ActionEvent event) { }
+  public void func2ButtonAction(ActionEvent event) { }
+  public void func3ButtonAction(ActionEvent event) { }
+  public void func4ButtonAction(ActionEvent event) { }
 
-    public void func1ButtonAction(ActionEvent event) { }
-    public void func2ButtonAction(ActionEvent event) { }
-    public void func3ButtonAction(ActionEvent event) { }
-    public void func4ButtonAction(ActionEvent event) { }
+  /* keyActionメソッド: 各キーが押された時の処理 */
 
-    public void keyAction(KeyEvent event){
-        KeyCode key = event.getCode();
-        if (key == KeyCode.DOWN){
-            downButtonAction();
-        }else if (key == KeyCode.RIGHT){
-            rightButtonAction();
-        }
+  public void keyAction(KeyEvent event){
+    KeyCode key = event.getCode();
+    if (key == KeyCode.UP){
+      upButtonAction();
+    } else if (key == KeyCode.LEFT){
+      leftButtonAction();
+    } else if (key == KeyCode.RIGHT){
+      rightButtonAction();
+    } else if (key == KeyCode.DOWN){
+      downButtonAction();
+    } else if (key == KeyCode.G){
+      goalButtonAction();
     }
+  }
 
-    public void outputAction(String actionString) {
-        System.out.println("Select Action: " + actionString);
-    }
+  public void outputAction(String actionString) {
+    System.out.println("Select Action: " + actionString);
+  }
 
-    public void downButtonAction(){
-        outputAction("DOWN");
-        chara.setCharaDir(MoveChara.TYPE_DOWN);
-        chara.move(0, 1);
-        mapPrint(chara, mapData);
-    }
-    public void downButtonAction(ActionEvent event) {
-        downButtonAction();
-    }
+  /* keyActionメソッドの各キーに対応する、各キャラの挙動 */
 
-    public void rightButtonAction(){
-        outputAction("RIGHT");
-        chara.setCharaDir(MoveChara.TYPE_RIGHT);
-        chara.move( 1, 0);
-        mapPrint(chara, mapData);
+  public void upButtonAction(){
+    outputAction("UP");
+    chara.setCharaDir(MoveChara.TYPE_UP);
+    chara.move(0, -1);  // 進めたか否かをtrueかfalseの値で返す
+    mapPrint(chara, mapData);
+  }
+  public void upButtonAction(ActionEvent event) {
+    upButtonAction();
+  }
+
+  public void leftButtonAction(){
+    outputAction("LEFT");
+    chara.setCharaDir(MoveChara.TYPE_LEFT);
+    chara.move(-1, 0);
+    mapPrint(chara, mapData);
+  }
+  public void leftButtonAction(ActionEvent event) {
+    leftButtonAction();
+  }
+
+  public void rightButtonAction(){
+    outputAction("RIGHT");
+    chara.setCharaDir(MoveChara.TYPE_RIGHT);
+    chara.move( 1, 0);
+    mapPrint(chara, mapData);
+  }
+  public void rightButtonAction(ActionEvent event) {
+    rightButtonAction();
+  }
+
+  public void downButtonAction(){
+    outputAction("DOWN");
+    chara.setCharaDir(MoveChara.TYPE_DOWN);
+    chara.move(0, 1);
+    mapPrint(chara, mapData);
+  }
+  public void downButtonAction(ActionEvent event) {
+    downButtonAction();
+  }
+
+  /**/
+  public void goalButtonAction(){
+    outputAction("GOAL");
+    makeNewMapAction();
+    mapPrint(chara, mapData);
+  }
+  public void goalButtonAction(ActionEvent event) {
+    goalButtonAction();
+  }
+
+  /*  */
+
+  /* 新規Ｍapを作成（） */
+  public void makeNewMapAction(){
+    mapData = new MapData(21,15);
+    chara = new MoveChara(1,1,mapData);
+    mapImageViews = new ImageView[mapData.getHeight()*mapData.getWidth()];
+    for(int y=0; y<mapData.getHeight(); y++){
+      for(int x=0; x<mapData.getWidth(); x++){
+        int index = y*mapData.getWidth() + x;
+        mapImageViews[index] = mapData.getImageView(x,y);
+      }
     }
-    public void rightButtonAction(ActionEvent event) {
-        rightButtonAction();
-    }
+    mapPrint(chara, mapData);
+  }
+  public void makeNewMapAction(ActionEvent event){
+    makeNewMapAction();
+  }
 }
