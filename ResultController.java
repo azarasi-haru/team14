7d31ee43a6cc7d9874753aad581bb356efe8be04
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2020-01-29 18:56:34 +0900

    編集

--- ResultController.java
+++ ResultController.java
@@ -5,13 +5,9 @@ import javafx.scene.control.Button;
 
 public class ResultController {
 
-    @FXML private Label firstLabel;
-    @FXML private Label secondLabel;
-    @FXML private Label thirdLabel;
-    @FXML private Label fourthLabel;
-    @FXML private Label fifthLabel;
+    @FXML public Label scoreLabel;
 
-    @FXML private Button closeButton;
+    @FXML public Button closeButton;
 
     public void closeButtonAction(ActionEvent event) {
         System.exit(0);
